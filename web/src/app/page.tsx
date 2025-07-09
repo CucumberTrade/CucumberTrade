47a22f710c6c366457854de05cc1f35afc3d30db
Cucumber Trade <hello@cucumber.trade>
2025-07-09 20:30:45 +0200

    feat: Cucumber Trade

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <main className="min-h-screen grid place-items-center text-4xl font-bold">
-      Hello&nbsp;World
+    <main className="min-h-screen grid place-items-center font-heading text-6xl font-extrabold">
+      CUCUMBER
+      <span className="text-[#59ED6C]">TRADE</span>
     </main>
   );
 }
