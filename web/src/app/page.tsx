cefe0051569d25adfc416670b412bdfba13f51de
Cucumber Trade <hello@cucumber.trade>
2025-07-09 21:29:25 +0200

    chore: remove old web app

--- web/src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main className="min-h-screen grid place-items-center font-heading text-6xl font-extrabold">
-      CUCUMBER
-      <span className="text-[#59ED6C]">TRADE</span>
-    </main>
-  );
-}
