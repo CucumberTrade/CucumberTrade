098d83116ad57272d8cc599ef8f86cb9c5ff310a
CucumberTrade <hello@cucumber.trade>
2025-07-28 14:01:37 +0200

    Delete web directory

--- web/src/main.tsx
+++
@@ -1,9 +0,0 @@
-// src/main.tsx
-
-import ReactDOM from 'react-dom/client';
-import App from './App';      // <- import the default export
-import './App.css';           // <- import your CSS
-
-ReactDOM
-  .createRoot(document.getElementById('root')!)
-  .render(<App />);
